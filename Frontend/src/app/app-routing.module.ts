891bab666f987dcf43aa59c4d860aeb0bb5f322a
Traqnar <nariman.hasanpourarba@student.vinci.be>
2022-12-08 12:27:53 +0100

    merge

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'home', component: HomepageComponent},
 
-const routes: Routes = [];
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
