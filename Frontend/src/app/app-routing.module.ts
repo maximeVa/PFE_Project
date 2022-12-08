1903dec15ccca083e54aa29642eaf735c98ca5c9
maximeVa <maxime.vanachter@student.vinci.be>
2022-12-08 15:07:28 +0100

    add - loginPage + css without fetch

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+
 ];
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
