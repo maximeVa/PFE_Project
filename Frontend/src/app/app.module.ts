ad998adbda1f3cca9ce907aff1c0764f0941312f
maximeVa <maxime.vanachter@student.vinci.be>
2022-12-09 16:34:54 +0100

    Big commit, add RegisterPage, optimise and finish loginPage + optimise aboutPage

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,17 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
