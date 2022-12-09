44963c25b93794609e3e9c31e5a0292536cf3844
Traqnar <nariman.hasanpourarba@student.vinci.be>
2022-12-09 18:56:28 +0100

    Imported anguar/material + homepage finished + some animation added on homepage + on aboutpage + changed the navbar

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,8 +7,9 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomepageComponent } from './homepage/homepage.component';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,9 @@ import { HomepageComponent } from './homepage/homepage.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
