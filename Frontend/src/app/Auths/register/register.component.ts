08f693f7b9fc2161d88d50b6fa01db3e0ad72521
maximeVa <maxime.vanachter@student.vinci.be>
2022-12-10 17:45:16 +0100

    fix probleme lancement

--- Frontend/src/app/Auths/register/register.component.ts
+++ Frontend/src/app/Auths/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NewUser } from '../../models/NewUser';
+import { NewUser } from '../models/NewUser';
 
 @Component({
   selector: 'app-register',
