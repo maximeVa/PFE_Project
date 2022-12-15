f1c1f04bc8b07c4922e75c678a121b533e3f48b7
leolamien <90797794+leolamien@users.noreply.github.com>
2022-12-15 19:12:32 +0100

    Add filter in archive page

--- Frontend/src/app/archives/archives.component.ts
+++ Frontend/src/app/archives/archives.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {ArchivesService} from "./archives.service";
 
 @Component({
   selector: 'app-archives',
@@ -8,19 +8,79 @@ import { HttpClient } from '@angular/common/http';
 
 })
 export class ArchivesComponent implements OnInit {
-  games: any[] =[];
+  games: any[] = [];
+  gamesArray: any[] = [];
+  division: any[] = [];
+  coupe: any[] = [];
+  club: any[] = [];
+  competition: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private archivesService: ArchivesService) {
+  }
 
-  ngOnInit() {;
-    this.http.get('https://kickervinci.azurewebsites.net/games')
-  .subscribe((data: Object) => {
-    this.games = data as any[];
-    console.log(data)
-  });
+  ngOnInit(): void {
+    this.sortBy("")
+  }
 
-
-
-
+  sortBy(value: string): void {
+    this.archivesService.getArchives()
+    .subscribe(archives => this.gamesArray = archives);
+    this.games = [];
+    if (value === "coupe") {
+      this.gamesArray.forEach((data) => {
+        if (data.cup != null) {
+          this.coupe.push(data.cup.name);
+        }
+      })
+      this.coupe.forEach((coupe) => {
+        this.gamesArray.forEach((data) => {
+          if (data.cup != null && data.cup.name === coupe) {
+            this.games.push(data);
+          }
+        })
+      })
+      this.games = [...new Set(this.games)];
+    } else if (value === "competition") {
+      this.gamesArray.forEach((data) => {
+        this.competition.push(data.competition.date);
+      })
+      this.competition.forEach((competition) => {
+        this.gamesArray.forEach((data) => {
+          if (data.competition.date === competition) {
+            this.games.push(data);
+          }
+        })
+      })
+      this.games = [...new Set(this.games)];
+    } else if (value === "club") {
+      this.gamesArray.forEach((data) => {
+        this.club.push(data.guestClub.name);
+      })
+      this.club.forEach((club) => {
+        this.gamesArray.forEach((data) => {
+          if (data.guestClub.name === club) {
+            this.games.push(data);
+          }
+        })
+      })
+      this.games = [...new Set(this.games)];
+    } else if (value === "division") {
+      this.archivesService.getArchives()
+      .subscribe(archives => this.gamesArray = archives);
+      this.gamesArray.forEach((data) => {
+        this.division.push(data.guestClub.division.name);
+      })
+      this.division.forEach((division) => {
+        this.gamesArray.forEach((data) => {
+          if (data.guestClub.division.name === division) {
+            this.games.push(data);
+          }
+        })
+      })
+      this.games = [...new Set(this.games)];
+    } else {
+      this.archivesService.getArchives()
+      .subscribe(archives => this.games = archives);
+    }
   }
 }
